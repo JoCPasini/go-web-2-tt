d662b83bcbd7d74aa306e5e03cc0a4cde5d8ea62
Jose Pasini <jose.pasini@mercadolibre.com>
2021-12-27 17:00:40 -0300

    .

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/JosePasiniMercadolibre/go-web-2-tt/cmd/server/handler"
 	"github.com/JosePasiniMercadolibre/go-web-2-tt/internal/transacciones"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	//_ = godotenv.Load()
+
+	err := godotenv.Load()
+	fmt.Println(os.Getenv("TOKEN"))
+	if err != nil {
+		fmt.Print(err.Error())
+	} else {
+		fmt.Println("************************", os.Getenv("TOKEN"))
+	}
+
 	repo := transacciones.NewRepository()
 	service := transacciones.NewService(repo)
 	handler := handler.NewTransaccion(service)
