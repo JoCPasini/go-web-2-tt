aedaac6778b79fcedea16956d4b48ee832d0b754
Jose Pasini <jose.pasini@mercadolibre.com>
2021-12-28 14:01:11 -0300

    codigo listo para comenzar con swagger

--- cmd/server/handler/transacciones.go
+++ cmd/server/handler/transacciones.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/JosePasiniMercadolibre/go-web-2-tt/internal/transacciones"
+	"github.com/JosePasiniMercadolibre/go-web-2-tt/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,38 +32,29 @@ func NewTransaccion(t transacciones.Service) *Transaccion {
 
 func (t *Transaccion) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		/*
-			tokem := ctx.Request.Header.Get("token")
-			if tokem != os.Getenv("TOKEN") {
-				ctx.JSON(400, gin.H{
-					"error": "Token Inválido",
-				})
-				return
-			}
-		*/
+
+		tokem := ctx.Request.Header.Get("token")
+		if tokem != os.Getenv("TOKEN") {
+			ctx.JSON(400, web.NewResponse(400, nil, "Token inválido"))
+			return
+		}
 
 		transacciones, err := t.service.GetAll()
 		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": "No se encontró",
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, string(err.Error())))
 			return
 		}
-		ctx.JSON(200, transacciones)
+		ctx.JSON(200, web.NewResponse(200, transacciones, ""))
 	}
 }
 
 func (t *Transaccion) Store() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		/*
-			tokem := ctx.Request.Header.Get("token")
-			if tokem != os.Getenv("TOKEN") {
-				ctx.JSON(400, gin.H{
-					"error": "Token Inválido",
-				})
-				return
-			}
-		*/
+		tokem := ctx.Request.Header.Get("token")
+		if tokem != os.Getenv("TOKEN") {
+			ctx.JSON(400, web.NewResponse(400, nil, "Token inválido"))
+			return
+		}
 		var req request
 
 		if err := ctx.Bind(&req); err != nil {
@@ -80,15 +73,11 @@ func (t *Transaccion) Store() gin.HandlerFunc {
 }
 func (t *Transaccion) Update() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		/*
-			tokem := ctx.Request.Header.Get("token")
-			if tokem != os.Getenv("TOKEN") {
-				ctx.JSON(400, gin.H{
-					"error": "Token Inválido",
-				})
-				return
-			}
-		*/
+		tokem := ctx.Request.Header.Get("token")
+		if tokem != os.Getenv("TOKEN") {
+			ctx.JSON(400, web.NewResponse(400, nil, "Token inválido"))
+			return
+		}
 		idParam, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 
 		if err != nil {
@@ -105,39 +94,39 @@ func (t *Transaccion) Update() gin.HandlerFunc {
 
 		// Condicional para verificar que el campo no venga vacío
 		if req.CodigoTransaccion == "" {
-			ctx.JSON(400, gin.H{"error": "El codigo de la transaccion es requerido, no puede ir un campo vacío, debe modificar todo"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Error, el código de transacción no puede ir vacío"))
 			return
 		}
 
 		if req.Moneda == "" {
-			ctx.JSON(400, gin.H{"error": "La moneda de la transaccion es requerida, no puede ir un campo vacío, debe modificar todo"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Error, la moneda no puede ir vacía"))
 			return
 		}
 
 		if req.Monto == 0 {
-			ctx.JSON(400, gin.H{"error": "El monto de la transaccion es requerido, no puede ir un campo vacío, debe modificar todo"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Error, el monto no puede ir vacío"))
 			return
 		}
 
 		if req.Emisor == "" {
-			ctx.JSON(400, gin.H{"error": "El emisor de la transaccion es requerido, no puede ir un campo vacío, debe modificar todo"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Error, el emisor no puede ir vacío"))
 			return
 		}
 
 		if req.Receptor == "" {
-			ctx.JSON(400, gin.H{"error": "El receptor de la transaccion es requerido, no puede ir un campo vacío, debe modificar todo"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Error, el receptor no puede ir vacío"))
 			return
 		}
 
 		if req.FechaTransaccion == "" {
-			ctx.JSON(400, gin.H{"error": "La fecha de la transaccion es requerido, no puede ir un campo vacío, debe modificar todo"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Error, la fecha de transacción no puede ir vacío"))
 			return
 		}
 
 		t1, err := t.service.Update(int(idParam), req.CodigoTransaccion, req.Moneda, req.Monto, req.Emisor, req.Receptor, req.FechaTransaccion)
 
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, string(err.Error())))
 			return
 		}
 		ctx.JSON(200, t1)
@@ -147,26 +136,23 @@ func (t *Transaccion) Update() gin.HandlerFunc {
 
 func (t *Transaccion) UpdateCodigoMonto() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		/*
-			tokem := ctx.Request.Header.Get("token")
-			if tokem != os.Getenv("TOKEN") {
-				ctx.JSON(400, gin.H{
-					"error": "Token Inválido",
-				})
-				return
-			}
-		*/
+		tokem := ctx.Request.Header.Get("token")
+		if tokem != os.Getenv("TOKEN") {
+			ctx.JSON(400, web.NewResponse(400, nil, "Token inválido"))
+			return
+		}
 		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "id invalido"})
+			ctx.JSON(400, web.NewResponse(400, nil, "Id inválido"))
+
 			return
 		}
 
 		var req request
 
 		if err := ctx.Bind(&req); err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, string(err.Error())))
 			return
 		}
 
@@ -186,30 +172,26 @@ func (t *Transaccion) UpdateCodigoMonto() gin.HandlerFunc {
 			ctx.JSON(400, gin.H{"error": "Id no encontrado"})
 		}
 
-		ctx.JSON(200, t1)
+		ctx.JSON(200, web.NewResponse(200, t1, ""))
 
 	}
 }
 
 func (t *Transaccion) Delete() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		/*
-			tokem := ctx.Request.Header.Get("token")
-			if tokem != os.Getenv("TOKEN") {
-				ctx.JSON(400, gin.H{
-					"error": "Token Inválido",
-				})
-				return
-			}
-		*/
+		tokem := ctx.Request.Header.Get("token")
+		if tokem != os.Getenv("TOKEN") {
+			ctx.JSON(400, web.NewResponse(400, nil, "Token inválido"))
+			return
+		}
 		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, string(err.Error())))
 			return
 		}
 		err = t.service.Delete(int(id))
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, string(err.Error())))
 			return
 		}
 		ctx.JSON(200, gin.H{
